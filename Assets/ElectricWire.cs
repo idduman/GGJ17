9c2501324578fd369e7902a55ebe069fec884636
ILHAN DORUK DUMAN <e1819267@ceng.metu.edu.tr>
2017-01-21 17:15:12 +0300

    New shit

--- Assets/ElectricWire.cs
+++ Assets/ElectricWire.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum Shape { Sine, Square, Triangle, Flat }
+
 public class ElectricWire : MonoBehaviour
 {
 
@@ -8,11 +10,16 @@ public class ElectricWire : MonoBehaviour
     public Transform StartPoint, EndPoint;
     public int SampleRate = 300;
     public int SegmentCount = 3;
+    public Shape waveform;
+    [HideInInspector]
+    public int phase,frequency,score;
     public float swidth = 0.1f;
     public float ewidth = 0.1f;
-    private enum Shape {Sine,Square,Triangle,Flat}
-    private Shape waveform;
-    private float phase,frequency,magnitude;
+    public float magrate = 1f;
+    [HideInInspector]
+    public float magnitude,magnitude_max,pingpong;
+    [HideInInspector]
+    public bool pass;
     private Vector3[] points;
     private Vector3 startPos, endPos;
     private float distance;
@@ -20,13 +27,16 @@ public class ElectricWire : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        score = 0;
         phase = 0;
-        frequency = 1;
-        magnitude = StartPoint.lossyScale.y / 2;
+        frequency = 2;
+        pingpong = 0;
+        pass = false;
+        magnitude_max = (StartPoint.lossyScale.y / 2) - 0.15f;
         startPos = StartPoint.position; //position initialization
-        startPos.y -= magnitude;
+        startPos.y -= magnitude_max;
         endPos = EndPoint.position;
-        endPos.y -= magnitude;
+        endPos.y -= magnitude_max;
         distance = Mathf.Abs(endPos.x - startPos.x);
         lineRenderer = GetComponent<LineRenderer>();
         points = new Vector3[SampleRate];
@@ -40,41 +50,43 @@ public class ElectricWire : MonoBehaviour
         switch (waveform)
         {
             case Shape.Sine:
-                phaseShift = (phase - 1f) * (Mathf.PI / 2);
+                phaseShift = (phase - 0.5f) * Mathf.PI;
                 for (int i = 0; i < SampleRate; i++)
                 {
-                    t = Mathf.Lerp(0, 2 * frequency * SegmentCount * Mathf.PI, (float)i / SampleRate);
+                    t = Mathf.Lerp(0, frequency * SegmentCount * Mathf.PI, (float)i / SampleRate);
                     points[i] = Vector3.Lerp(startPos, endPos, (float)i / (SampleRate - 1));
-                    points[i].y += magnitude * (1 + Mathf.Sin(t + phaseShift));
+                    points[i].y += (magnitude + pingpong) * (1 + Mathf.Sin(t + phaseShift)) + Random.Range(-0.1f, 0.1f); //POINT SET
                 }
                 break;
             case Shape.Square:
-                phaseShift = phase * (Mathf.PI);
+                phaseShift = phase * Mathf.PI;
                 for (int i = 0; i < SampleRate; i++)
                 {
-                    t = Mathf.Lerp(0, 2 * frequency * SegmentCount * Mathf.PI, (float)i / SampleRate);
+                    t = Mathf.Lerp(0, frequency * SegmentCount * Mathf.PI, (float)i / SampleRate);
                     points[i] = Vector3.Lerp(startPos, endPos, (float)i / (SampleRate - 1));
-                    x = (((t + phaseShift) / Mathf.PI) % 4);
-                    if (x < 2)
+                    x = (((t + phaseShift) / Mathf.PI) % 2);
+                    if (x > 1)
                     {
-                        points[i].y += magnitude * 2;
+                        points[i].y += (magnitude + pingpong) * 2 + Random.Range(-0.1f, 0.1f); //POINT SET
                     }
+                    else
+                        points[i].y += Random.Range(-0.1f, 0.1f); // POINT SET
                 }
                 break;
             case Shape.Triangle:
-                phaseShift = phase * (Mathf.PI / 2);
+                phaseShift = phase * Mathf.PI;
                 for (int i = 0; i < SampleRate; i++)
                 {
-                    t = Mathf.Lerp(0, 2 * frequency * SegmentCount * Mathf.PI, (float)i / SampleRate);
+                    t = Mathf.Lerp(0, frequency * SegmentCount * Mathf.PI, (float)i / SampleRate);
                     points[i] = Vector3.Lerp(startPos, endPos, (float)i / (SampleRate - 1));
                     x = (((t + phaseShift) / Mathf.PI) % 2);
                     if (x < 1)
                     {
-                        points[i].y += magnitude * 2 * x;
+                        points[i].y += (magnitude + pingpong) * 2 * x + Random.Range(-0.1f, 0.1f); //POINT SET
                     }
                     else
                     {
-                        points[i].y += magnitude * 2 * (2 - x);
+                        points[i].y += (magnitude + pingpong) * 2 * (2 - x) + Random.Range(-0.1f, 0.1f); //POINT SET
                     }
                 }
                 break;
@@ -83,7 +95,7 @@ public class ElectricWire : MonoBehaviour
         lineRenderer.SetPositions(points);
     }
 
-    void SetSineWave()
+    /*void SetSineWave()
     {
         float phaseShift = (phase - 0.75f) * (Mathf.PI / 2);
         for (int i=0; i<SampleRate; i++)
@@ -134,29 +146,72 @@ public class ElectricWire : MonoBehaviour
         }
         lineRenderer.SetVertexCount(SampleRate);
         lineRenderer.SetPositions(points);
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
-            //currentSegmentCount /= 2;
-            frequency /= 2;
+        {
+            if(frequency > 1)
+                frequency /= 2;
+        }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            //currentSegmentCount *= 2;
-            frequency *= 2;
+            if(frequency < 4)
+                frequency *= 2;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            phase += 2;
+            phase++;
         }
-        SetWave();
-        if (Input.GetKey(KeyCode.Q))
-            waveform = Shape.Sine;
-        if (Input.GetKey(KeyCode.W))
-            waveform = Shape.Square;
-        if (Input.GetKey(KeyCode.E))
-            waveform = Shape.Triangle;
+        if (Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.E))
+        {
+            if (waveform != Shape.Sine)
+            {
+                magnitude = 0;
+                waveform = Shape.Sine;
+            }
+            else
+                magnitude += magrate* 2 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E))
+        {
+            if (waveform != Shape.Square)
+            {
+                magnitude = 0;
+                waveform = Shape.Square;
+            }
+            else
+                magnitude += magrate * 2 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.E) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Q))
+        {
+            if (waveform != Shape.Triangle)
+            {
+                magnitude = 0;
+                waveform = Shape.Triangle;
+            }
+            else
+                magnitude += magrate * 2 * Time.deltaTime;
+        }
+
+        magnitude = Mathf.Clamp(magnitude, 0, magnitude_max);
+
+        if (magnitude > 0.98 * magnitude_max)
+        {
+            pass = true;
+            pingpong += 0.1f;
+            pingpong = Mathf.PingPong(5 * Time.time, 0.3f);
+        }
+        else
+        {
+            pass = false;
+            pingpong = 0;
+        }
+
+        SetWave(); //CALL THE FUNCTION THAT SETS THE LINE RENDERER
+
+        magnitude -= magrate * Time.deltaTime;
     }
 }
---
+++ Assets/Minion.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minion : MonoBehaviour
+{
+    public Transform Checkpoint_Left, Checkpoint_Right;
+    public ElectricWire wire;
+    [HideInInspector]
+    public Shape waveform;
+    public float speed = 1f;
+    private int phase,frequency;
+    private bool active;
+    private int lane;
+    private float diff;
+
+	// Use this for initialization
+	void Start ()
+    {
+        diff = Checkpoint_Right.position.x - Checkpoint_Left.position.x;
+        lane = 0;
+        active = true;
+        waveform = Shape.Sine;
+        phase = 0;
+        frequency = 2;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        float laneX = Checkpoint_Left.position.x + diff * (lane + 0.5f) / 3;
+        transform.position = new Vector3(laneX,transform.position.y, transform.position.z - speed * Time.deltaTime);
+        if (active && (transform.position.z <= Checkpoint_Left.position.z))
+        {
+            active = false;
+            HitGate();
+        }
+        else if(transform.position.z <= (Checkpoint_Left.position.z - 2))
+            Destroy(this.gameObject);
+
+        if (Input.GetKeyDown("1"))
+            lane = 0;
+        else if (Input.GetKeyDown("2"))
+            lane = 1;
+        else if (Input.GetKeyDown("3"))
+            lane = 2;
+    }
+
+    void HitGate()
+    {
+        if (wire.pass && waveform == wire.waveform && phase == wire.phase && frequency == wire.frequency)
+            wire.score += 1;
+        else
+            Destroy(this.gameObject);
+    }
+}
