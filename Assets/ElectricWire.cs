d51ec974e57eb90d9c8d791b8fa29e8a83eb86b0
ILHAN DORUK DUMAN <e1819267@ceng.metu.edu.tr>
2017-01-22 15:06:03 +0300

    New Initial commit.

--- Assets/ElectricWire.cs
+++ Assets/ElectricWire.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public enum Shape { Sine, Square, Triangle, Flat }
 
 public class ElectricWire : MonoBehaviour
 {
-    public GameObject minion_prefab;
+    public Text scoreText;
+    public Material Electric_Sine, Electric_Square, Electric_Triangle;
+    public GameObject minion_pink,minion_green,minion_purple;
+    public GameObject smoke,shock;
     LineRenderer lineRenderer;
     public Transform StartPoint, EndPoint;
     public int SampleRate = 300;
@@ -21,6 +25,9 @@ public class ElectricWire : MonoBehaviour
     public float magnitude,magnitude_max,pingpong;
     [HideInInspector]
     public bool pass;
+    public AudioSource Buzz;
+    [HideInInspector]
+    public int health;
     private Vector3[] points;
     private Vector3 startPos, endPos;
     private float distance,timer;
@@ -33,9 +40,9 @@ public class ElectricWire : MonoBehaviour
         phase = 0;
         frequency = 1;
         pingpong = 0;
-        timer = 0;
+        timer = 1;
         pass = false;
-        magnitude_max = (StartPoint.lossyScale.y / 2) - 0.15f;
+        magnitude_max = (StartPoint.localScale.y / 2) - 0.15f;
         startPos = StartPoint.position; //position initialization
         startPos.y -= magnitude_max;
         endPos = EndPoint.position;
@@ -45,6 +52,8 @@ public class ElectricWire : MonoBehaviour
         points = new Vector3[SampleRate];
         lineRenderer.SetWidth(swidth, ewidth);
         waveform = Shape.Sine;
+        health = 15;
+        scoreText.text = "0";
     }
 
     void Update()
@@ -56,12 +65,13 @@ public class ElectricWire : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if(frequency < 4)
+            if(frequency < 2)
                 frequency *= 2;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             phase++;
+            phase %= 2;
         }
         if (Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.E))
         {
@@ -69,6 +79,7 @@ public class ElectricWire : MonoBehaviour
             {
                 magnitude = 0;
                 waveform = Shape.Sine;
+                lineRenderer.material = Electric_Sine;
             }
             else
                 magnitude += magrate* 2 * Time.deltaTime;
@@ -79,6 +90,7 @@ public class ElectricWire : MonoBehaviour
             {
                 magnitude = 0;
                 waveform = Shape.Square;
+                lineRenderer.material = Electric_Square;
             }
             else
                 magnitude += magrate * 2 * Time.deltaTime;
@@ -89,6 +101,7 @@ public class ElectricWire : MonoBehaviour
             {
                 magnitude = 0;
                 waveform = Shape.Triangle;
+                lineRenderer.material = Electric_Triangle;
             }
             else
                 magnitude += magrate * 2 * Time.deltaTime;
@@ -115,6 +128,8 @@ public class ElectricWire : MonoBehaviour
         }
         timer += Time.deltaTime;
 
+        Buzz.volume = 0.02f + 0.07f * (magnitude / magnitude_max);
+
         SetWave(); //CALL THE FUNCTION THAT SETS THE LINE RENDERER
 
         magnitude -= magrate * Time.deltaTime;
@@ -131,7 +146,7 @@ public class ElectricWire : MonoBehaviour
                 {
                     t = Mathf.Lerp(0, frequency * 2 * SegmentCount * Mathf.PI, (float)i / SampleRate);
                     points[i] = Vector3.Lerp(startPos, endPos, (float)i / (SampleRate - 1));
-                    points[i].y += (magnitude + pingpong) * (1 + Mathf.Sin(t + phaseShift)) + Random.Range(-0.01f, 0.01f); //POINT SET
+                    points[i].y += (magnitude + pingpong) * 0.8f * (1 + Mathf.Sin(t + phaseShift)) + Random.Range(-0.01f, 0.01f); //POINT SET
                 }
                 break;
             case Shape.Square:
@@ -174,10 +189,40 @@ public class ElectricWire : MonoBehaviour
     void SpawnMinion()
     {
         Quaternion rot = Quaternion.identity;
-        rot.eulerAngles = new Vector3(0,180,0);
-        m = (GameObject)Instantiate(minion_prefab, new Vector3(0, 0.05f, 3), rot);
+        rot.eulerAngles = new Vector3(0, 180, 0);
+        int w = Random.Range(0, 3);
+        switch (w)
+        {
+            case 0:
+                m = (GameObject)Instantiate(minion_green, new Vector3(0, 0.05f, 3), rot);
+                m.GetComponent<Minion>().waveform = Shape.Sine;
+                break;
+            case 1:
+                m = (GameObject)Instantiate(minion_pink, new Vector3(0, 0.05f, 3), rot);
+                m.GetComponent<Minion>().waveform = Shape.Square;
+                break;
+            case 2:
+                m = (GameObject)Instantiate(minion_purple, new Vector3(0, 0.05f, 3), rot);
+                m.GetComponent<Minion>().waveform = Shape.Triangle;
+                break;
+        }
         m.GetComponent<Minion>().Checkpoint_Left = StartPoint;
         m.GetComponent<Minion>().Checkpoint_Right = EndPoint;
         m.GetComponent<Minion>().wire = this;
+    }
+
+    public void Score(bool pass)
+    {
+        if (pass)
+        {
+            health++;
+            score++;
+            scoreText.text = score.ToString();
+        }
+        else
+        {
+            health -= 5;
+        }
+        Debug.Log("Score: " + score);
     }
 }
--- Assets/Minion.cs
+++ Assets/Minion.cs
@@ -17,13 +17,18 @@ public class Minion : MonoBehaviour
     private float diff,laneX;
     private Vector3[] points;
     private Vector3 startPos, endPos;
-    private bool CoroutineDone;
+    private bool CoroutineDone,isDead;
     private float horLerpTime = 1f;
     private float currentHorLerpTime;
     private float verLerpTime = 0.5f;
     private float currentVerLerpTime;
     private float minionY;
+    private float deathTimer, deathTimerMax;
     private Animator anim;
+    private GameObject smoke,shock;
+    public Texture shockTexture;
+    public MeshRenderer surat;
+    public GameObject zapSkeletonObj;
 
     public float LaneX
     {
@@ -37,7 +42,7 @@ public class Minion : MonoBehaviour
 	void Start ()
     {
         magnitude = 0.2f;
-        SampleRate = 100;
+        SampleRate = 150;
         minionY = 0.05f;
         diff = Checkpoint_Right.position.x - Checkpoint_Left.position.x;
         lane = Random.Range(0, 3);
@@ -45,50 +50,41 @@ public class Minion : MonoBehaviour
         frequency = Random.Range(1, 3);
         Cable = GetComponentInChildren<LineRenderer>();
         anim = GetComponent<Animator>();
+        deathTimer = 0;
+        deathTimerMax = 1.5f;
+        smoke = null;
+        shock = null;
 
-        int w = Random.Range(0, 3);
-        switch(w)
-        {
-            case 0:
-                waveform = Shape.Sine;
-                break;
-            case 1:
-                waveform = Shape.Square;
-                break;
-            case 2:
-                waveform = Shape.Triangle;
-                break;
-        }
+        anim.SetBool("isDead", false);
+
         active = true;
         points = new Vector3[SampleRate+2];
         laneX = Checkpoint_Left.position.x + diff * (lane + 0.5f) / 3;
         newLane = lane;
         transform.position = new Vector3(laneX, 0.05f, 3f);
+        isDead = false;
         CoroutineDone = true;
     }
 	
 	// Update is called once per frame
 	void Update()
     {
-        if(newLane != lane)
-        {
-
-        }
-        else
-        {
-
-        }
-
-        transform.position = new Vector3(transform.position.x ,transform.position.y, transform.position.z - speed * Time.deltaTime);
         startPos = Anten_Left.position;
         endPos = Anten_Right.position;
-        if (active && (transform.position.z <= Checkpoint_Left.position.z))
+
+        if(!isDead)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed * Time.deltaTime);
+        }
+
+        if (active && !isDead && (transform.position.z <= Checkpoint_Left.position.z))
         {
             active = false;
             HitGate();
         }
-        else if(transform.position.z <= (Checkpoint_Left.position.z - 1))
+        else if(transform.position.z <= (Checkpoint_Left.position.z - 2))
             Destroy(this.gameObject);
+
 
         if (CoroutineDone && Input.GetKeyDown("1"))
             StartCoroutine(SwapLane(0));
@@ -96,13 +92,13 @@ public class Minion : MonoBehaviour
             StartCoroutine(SwapLane(1));
         else if (CoroutineDone && Input.GetKeyDown("3"))
             StartCoroutine(SwapLane(2));
+
         SetWave();
     }
 
     IEnumerator SwapLane(int destination)
     {
         CoroutineDone = false;
-        anim.SetBool("LJump", true);
         currentHorLerpTime = 0;
         lane = destination;
         float newX,jumpdist = 0;
@@ -124,7 +120,6 @@ public class Minion : MonoBehaviour
             yield return null;
         }
         transform.position = new Vector3(p.x, originalY, p.z);
-        anim.SetBool("LJump", false);
         CoroutineDone = true;
     }
 
@@ -132,13 +127,53 @@ public class Minion : MonoBehaviour
     {
         if (wire.pass && waveform == wire.waveform && phase == wire.phase && frequency == wire.frequency)
         {
-            Debug.Log("Pass");
-            wire.score += 1;
+            //Debug.Log("Pass");
+            wire.Score(true);
         }
         else
         {
-            Debug.Log("SHOCK!");
-            Destroy(this.gameObject);
+            //Debug.Log("SHOCK!");
+            anim.SetBool("isDead", true);
+            isDead = true;
+            wire.Score(false);
+            surat.material.mainTexture = shockTexture;
+            StartCoroutine(DeathCoroutine(1f));
+        }
+    }
+
+    IEnumerator DeathCoroutine(float duration)
+    {
+        shock = (GameObject)Instantiate(wire.shock, transform.position, transform.rotation);
+        shock.transform.Translate(0, 0.6f, 0);
+        StartCoroutine(ZapCoroutine(8));
+        yield return new WaitForSeconds(duration);
+        Destroy(shock);
+        Cable.gameObject.active = false;
+        smoke = (GameObject)Instantiate(wire.smoke, transform.position, transform.rotation);
+        foreach (var r in GetComponentsInChildren<MeshRenderer>())
+        {
+            r.enabled = false;
+        }
+        yield return new WaitForSeconds(duration);
+        Destroy(smoke);
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator ZapCoroutine(int count)
+    {
+        
+        while(count > 0)
+        {
+            zapSkeletonObj.SetActive(!zapSkeletonObj.activeInHierarchy);
+            foreach (var r in GetComponentsInChildren<MeshRenderer>())
+            {
+                if(r.gameObject != zapSkeletonObj)
+                {
+                    r.enabled = !zapSkeletonObj.activeInHierarchy;
+                }
+            }
+            count--;
+            yield return new WaitForSeconds(0.03f);
         }
     }
 
@@ -155,7 +190,7 @@ public class Minion : MonoBehaviour
                 {
                     t = Mathf.Lerp(0, frequency * 2 * Mathf.PI, (float)i / SampleRate);
                     points[i] = Vector3.Lerp(startPos, endPos, (float)i / (SampleRate - 1));
-                    points[i].y += magnitude * (1 + Mathf.Sin(t + phaseShift)); //POINT SET
+                    points[i].y += magnitude * 0.8f * (1 + Mathf.Sin(t + phaseShift)); //POINT SET
                 }
                 break;
             case Shape.Square:
